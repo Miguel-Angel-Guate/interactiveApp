403f9a0f6765819fb43b5bdf47e3eb0598836b1d
Miguel-Angel-Guate <miguelangelteletor@yahoo.es>
2024-10-10 22:17:49 +0200

    working the play and pause sound currenlty

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const messages = [
     "Please hold, as we load up the best experience for you...",
     "Loading content, please wait...",
     "You're all set!",
-    "Best Experience with the guys..",
-    "Turn the sound on",
+    "Are you ready to connect with God?",
+    "Turn the sound",
 ];
+const turnIndications = [
+    "On",
+    "Off",
+    "Pause",
+]
+const turnIndication = (position) => {
+    position = 0 ? turnIndications[0] : position === 1 ? turnIndications[1] : turnIndications[2];
+
+    console.log("🚀 ~ turnIndication ~ position:", position)
+    return position;
+}
 
 const firstPartMessages = messages.slice(0, 3);
 const secondPartMessages = messages.slice(3);
@@ -23,37 +34,44 @@ const appendParagraph = (message) => {
     const p = document.createElement('p');
     p.textContent = message;
     element.appendChild(p);
+
 };
 
-const appendImage = (parameter) => {
+const toggleAudio = (audio, img) => {
+    audio.paused ? (audio.play(), img.src = "./assets/no-sound.png") : (audio.pause(), img.src = "./assets/pause.png");
+};
+
+const appendImage = () => {
     const img = document.createElement("img");
     img.src = "./assets/spe.png";
     img.style.width = "80px";
 
     const imageElement = element.appendChild(img);
-    const eventoImage = imageElement.addEventListener("click", () => {
-         img.src = "./assets/no-sound.png"
-    });
-    return eventoImage;
+    const audio = new Audio("./assets/GodTime.mp3");
+    imageElement.addEventListener("click", () => {
+        toggleAudio(audio, imageElement);
 
+    });
 };
 
-const show2PartesMessage = (index = 0) => { 
-    index < firstPartMessages.length 
-      ? (
-          displayMessage(firstPartMessages[index]),
-          setTimeout(() => {
-            clearMessage(); 
-            show2PartesMessage(index + 1);
-          }, 1000)
-        )
-      : (
-          secondPartMessages.forEach(message => appendParagraph(message)),
-          !document.querySelector('#there img') && appendImage()
-        );
-  };
+const show2PartesMessage = (index = 0) => {
+    index < firstPartMessages.length ?
+        (
+            displayMessage(firstPartMessages[index]),
+            setTimeout(() => {
+                clearMessage();
+                show2PartesMessage(index + 1);
+            }, 1000)
+        ) :
+        (
 
- 
+            secondPartMessages.forEach(message => appendParagraph(message)),
+            appendParagraph(turnIndications[0]),
+            !document.querySelector('#there img') && appendImage()
+        );
+};
+
+
 window.addEventListener("load", (event) => {
     show2PartesMessage();
 });
